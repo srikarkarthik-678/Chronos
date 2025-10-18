224a351dbc73368c9208e71a6c6a754a577a2bf6
srikarkarthik-678 <srikarkarthik93@outlook.com>
2025-10-18 19:34:07 +0530

    Update page.jsx

--- app/user/input/page.jsx
+++ app/user/input/page.jsx
@@ -4,11 +4,16 @@ import BlurText from "@/app/react/animations/page";
 import { useResponse } from "@/app/context/ResponseContext";
 const Page = () => {
   const [oncompletion, setoncompletion] = useState(true);
+  const [loading, setLoading] = useState(true);
   const { responses } = useResponse();
   useEffect(() => {
     const timec = setTimeout(() => setoncompletion(false), 5000);
     return () => clearTimeout(timec);
   }, []);
+  useEffect(() => {
+    setLoading(false);
+    console.log("hello")
+  }, [responses]);
 
   return (
     <div className="mainsection text-white font-Ciz overflow-y-scroll h-[80vh] scrollbar-thumb-[#9b6b35] scrollbar-track-[#1a1a1a]">
@@ -24,21 +29,22 @@ const Page = () => {
             />
           </div>
         ) : (
-          <div className="conversation space-y-4 flex flex-col h-[50vh] justify-start">
-            {responses.length === 0 ? (
-              <p className="text-[#bfa887]">No inputs yet. Try sending one!</p>
+          <div className="conversation space-y-4 flex flex-col justify-start min-h-[200px]">
+            {loading ? (
+              <p className="text-[#ffdf9e] text-center text-xl animate-pulse mt-4">
+                Processing your input...
+              </p>
+            ) : responses.length === 0 ? (
+              <p className="text-[#bfa887]  text-md mt-4">No inputs yet. Try sending one!</p>
             ) : (
               responses.map((item, index) => (
-                <div 
-                  key={index} className="border border-[#9b6b35]/60 rounded-xl p-4 bg-black/30">
+                <div key={index} className="border border-[#9b6b35]/60 rounded-xl p-4 bg-black/30">
                   <p className="text-[#ffdf9e] font-semibold">
                     Your Input: <span className="font-normal">{item.message}</span>
                   </p>
-                  <p className="text-[#f5e6c8] mt-2 ">
+                  <p className="text-[#f5e6c8] mt-2">
                     Gemini Reconstruction:
-                    <span className="font-normal">
-                      {item.reconstructedText}
-                    </span>
+                    <span className="font-normal">{item.reconstructedText}</span>
                   </p>
                 </div>
               ))
