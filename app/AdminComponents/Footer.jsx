d8996348e33a7a71fd4a00b18928812c2d47e847
srikarkarthik-678 <srikarkarthik93@outlook.com>
2025-10-18 19:30:51 +0530

    Update Footer.jsx

--- app/AdminComponents/Footer.jsx
+++ app/AdminComponents/Footer.jsx
@@ -23,32 +23,38 @@ const Footer = () => {
     });
     const data = await res.json();
     setResponses((prev) => [
-      ...prev,{ message, reconstructedText: data.reconstructedText },
+      ...prev, { message, reconstructedText: data.reconstructedText },
     ]);
     setMessage("");
     setFile(null);
     setFileName("");
-    router.push("/user/input"); 
+    router.push("/user/input");
   };
-
+  const fileAdded = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFileName(file.name);
+      alert(`Your file "${file.name}" is added`); 
+    }
+  };
   return (
     <div className="footerdetails text-white">
       <div className="footercontent">
         <div className="footer backdrop-blur-md bg-black/40 border-t border-[#9b6b35]/70 px-6 py-3 flex items-center justify-between">
-          <label
-            htmlFor="fileInput"
-            className="flex items-center gap-2 cursor-pointer text-[#f5e6c8] hover:text-[#ffdf9e] transition-all duration-300 font-Ciz">
+          <input type="file" id="fileInput" className="hidden" onChange={fileAdded}
+          />
+          <label htmlFor="fileInput" className="flex items-center gap-2 cursor-pointer text-[#f5e6c8] hover:text-[#ffdf9e] transition-all duration-300 font-Ciz mr-5">
             <Paperclip className="w-5 h-5" />
-            <span className="text-sm tracking-wide">
+            <span className="text-sm max-sm:hidden">
               {fileName || "Attach File"}
             </span>
           </label>
-          <input id="fileInput" type="file" className="hidden" onChange={(e) => {setFile(e.target.files[0]); setFileName(e.target.files[0]?.name || "");}}/>
-          <input type="text" placeholder="Type your message" value={message} onChange={(e) => setMessage(e.target.value)}className="w-[70%] bg-transparent border border-[#9b6b35]/70 text-[#f5e6c8] placeholder-[#bfa887]/70 rounded-xl px-4 py-2 outline-none focus:border-[#ffdf9e] transition-all duration-300"/>
-          <button onClick={handleSubmit} className="ml-3 flex items-center gap-2 bg-[#9b6b35]/70 hover:bg-[#c48a44] text-[#f5e6c8] font-Ciz px-5 py-2 rounded-xl border border-[#ffdf9e]/40 shadow-md hover:shadow-[#ffdf9e]/40 transition-all duration-300">
+          <input id="fileInput" type="file" className="hidden" onChange={(e) => { setFile(e.target.files[0]); setFileName(e.target.files[0]?.name || ""); }} />
+          <input type="text" placeholder="Type your message" value={message} onChange={(e) => setMessage(e.target.value)} className="w-[70%] bg-transparent border border-[#9b6b35]/70 text-[#f5e6c8] placeholder-[#bfa887]/70 rounded-xl px-4 py-2 outline-none focus:border-[#ffdf9e] transition-all duration-300" />
+          <div onClick={handleSubmit} className="ml-3 flex items-center gap-2 bg-[#9b6b35]/70 hover:bg-[#c48a44] text-[#f5e6c8] font-Ciz px-5 py-2 rounded-xl border border-[#ffdf9e]/40 shadow-md hover:shadow-[#ffdf9e]/40 transition-all duration-300 max-sm:p-2 cursor-pointer">
             <Send className="w-4 h-4" />
-            Send
-          </button>
+            <span className="max-sm:hidden">Send</span>
+          </div>
         </div>
       </div>
     </div>
